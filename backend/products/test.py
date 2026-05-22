f24103836ce0faa98394056b4ac14a2f4bcd266f
snezhana_seliuk <nana-1980@mail.ru>
2026-05-22 21:00:50 +0300

    add test to app products

--- backend/products/test.py
+++ backend/products/test.py
@@ -8,12 +8,23 @@ from .models import Product
 
 @pytest.mark.django_db
 def test_product_model(product):
+    """
+    Тест проверки создания моделей услуг
+    :param product: fixture
+    """
     assert product.name == "TestProd"
     assert product.cost == 100
 
 
 @pytest.mark.django_db
 def test_view_product(client, user):
+    """
+    Тест проверки невозможности просмотра списка услуг неавторизованным пользователем.
+    Возвращает код 302 и перенаправляет на страницу входа.
+    :param client: fixture
+    :param user: fixture
+    :return: 302
+    """
     url = reverse("products:product-list")
     response = client.get(url)
     assert response.status_code == 302
@@ -22,6 +33,13 @@ def test_view_product(client, user):
 
 @pytest.mark.django_db
 def test_view_by_user(auth_user, user):
+    """
+    Тест проверки возможности просмотра списка услуг авторизованным пользователем.
+    Возвращает код 200 при наличии определенных разрешений.
+    :param client: fixture
+    :param user: fixture
+    :return: 200
+    """
     url = reverse("products:product-list")
     response = auth_user.get(url)
     assert response.status_code == 403
@@ -37,6 +55,13 @@ def test_view_by_user(auth_user, user):
 
 @pytest.mark.django_db
 def test_view_by_admin(auth_admin):
+    """
+    Тест проверки возможности просмотра списка услуг администратором.
+    Возвращает код 200.
+    :param client: fixture
+    :param user: fixture
+    :return: 200
+    """
     url = reverse("products:product-list")
     response = auth_admin.get(url)
     assert response.status_code == 200
@@ -45,6 +70,13 @@ def test_view_by_admin(auth_admin):
 
 @pytest.mark.django_db
 def test_view_detail_product(client, user, product):
+    """
+    Тест проверки невозможности просмотра деталей услуг неавторизованным пользователем.
+    Возвращает код 302 и перенаправляет на страницу входа.
+    :param client: fixture
+    :param user: fixture
+    :return: 302
+    """
     url = reverse("products:product-detail", kwargs={"pk":product.pk})
     response = client.get(url)
     assert response.status_code == 302
@@ -53,6 +85,13 @@ def test_view_detail_product(client, user, product):
 
 @pytest.mark.django_db
 def test_view_detail_by_user(auth_user, user, product):
+    """
+    Тест проверки возможности просмотра деталей услуг авторизованным пользователем.
+    Возвращает код 200 при наличии определенных разрешений.
+    :param client: fixture
+    :param user: fixture
+    :return: 200
+    """
     url = reverse("products:product-detail", kwargs={"pk": product.pk})
     response = auth_user.get(url)
     assert response.status_code == 403
@@ -68,6 +107,13 @@ def test_view_detail_by_user(auth_user, user, product):
 
 @pytest.mark.django_db
 def test_view_detail_by_admin(auth_admin, product):
+    """
+    Тест проверки возможности просмотра деталей  услуг администратором.
+    Возвращает код 200.
+    :param client: fixture
+    :param user: fixture
+    :return: 200
+    """
     url = reverse("products:product-detail", kwargs={"pk": product.pk})
     response = auth_admin.get(url)
     assert response.status_code == 200
@@ -76,6 +122,13 @@ def test_view_detail_by_admin(auth_admin, product):
 
 @pytest.mark.django_db
 def test_create_product(client, product):
+    """
+    Тест проверки невозможности создания услуги неавторизованным пользователем.
+    Возвращает код 302 и направляет на страницу входа.
+    :param client: fixture
+    :param product: fixture
+    :return: 302
+    """
     url = reverse("products:product-create")
     data = {
         "name":"NewProd",
@@ -89,6 +142,13 @@ def test_create_product(client, product):
 
 @pytest.mark.django_db
 def test_create_by_user(user, auth_user, product):
+    """
+    Тест проверки возможности создания услуги авторизованным пользователем.
+    Возвращает код 302 и создает услугу при наличии определенных прав.
+    :param client: fixture
+    :param product: fixture
+    :return: 302
+    """
     url = reverse("products:product-create")
     data = {
         "name": "NewProd",
@@ -111,6 +171,13 @@ def test_create_by_user(user, auth_user, product):
 
 @pytest.mark.django_db
 def test_create_by_admin(auth_admin, product):
+    """
+    Тест проверки возможности создания услуги администратором.
+    Возвращает код 302 и создает услугу.
+    :param client: fixture
+    :param product: fixture
+    :return: 302
+    """
     url = reverse("products:product-create")
     data = {
         "name": "NewProd",
@@ -128,6 +195,13 @@ def test_create_by_admin(auth_admin, product):
 
 @pytest.mark.django_db
 def test_update_product(client, product):
+    """
+    Тест проверки невозможности обновления услуги неавторизованным пользователем.
+    Возвращает код 302 и направляет на страницу входа.
+    :param client: fixture
+    :param product: fixture
+    :return: 302
+    """
     data = {
         "name": "UpdateProd",
         "description": "UpdateDesc",
@@ -141,6 +215,13 @@ def test_update_product(client, product):
 
 @pytest.mark.django_db
 def test_update_by_user(user, auth_user, product):
+    """
+    Тест проверки возможности обновления услуги авторизованным пользователем.
+    Возвращает код 302 и обновляет услугу при наличии определенных прав.
+    :param client: fixture
+    :param product: fixture
+    :return: 302
+    """
     data = {
         "name": "UpdateProd",
         "description": "UpdateDesc",
@@ -164,6 +245,13 @@ def test_update_by_user(user, auth_user, product):
 
 @pytest.mark.django_db
 def test_update_by_admin(auth_admin, product):
+    """
+    Тест проверки возможности обновления услуги администратором.
+    Возвращает код 302 и обновляет поля услуги.
+    :param client: fixture
+    :param product: fixture
+    :return: 302
+    """
     data = {
         "name": "UpdateProd",
         "description": "UpdateDesc",
@@ -178,6 +266,13 @@ def test_update_by_admin(auth_admin, product):
 
 @pytest.mark.django_db
 def test_delete_products(client, product):
+    """
+    Тест проверки невозможности удаления услуги неавторизованным пользователем.
+    Возвращает код 403.
+    :param client: fixture
+    :param product: fixture
+    :return: 403
+    """
     url = reverse("products:product-delete", kwargs={"pk":product.pk})
     response = client.post(url)
     assert response.status_code == 403
@@ -185,6 +280,13 @@ def test_delete_products(client, product):
 
 @pytest.mark.django_db
 def test_delete_by_user(user, auth_user, product):
+    """
+    Тест проверки невозможности удаления услуги авторизованным пользователем.
+    Возвращает код 403 даже при наличии прав.
+    :param client: fixture
+    :param product: fixture
+    :return: 403
+    """
     url = reverse("products:product-delete", kwargs={"pk":product.pk})
     response = auth_user.post(url)
     assert response.status_code == 403
@@ -200,12 +302,15 @@ def test_delete_by_user(user, auth_user, product):
 
 @pytest.mark.django_db
 def test_delete_by_admin(auth_admin, product):
+    """
+    Тест проверки возможности удаления услуги администратором.
+    Возвращает код 302 и направляет на страницу списка услуг.
+    :param client: fixture
+    :param product: fixture
+    :return: 302
+    """
     url = reverse("products:product-delete", kwargs={"pk":product.pk})
     response = auth_admin.post(url)
     assert response.status_code == 302
     assert not Product.objects.filter(name="TestProd").exists()
     assert Product.objects.count() == 0
-
-
-
-
