3895f114686fa5e4a2ba96b0de42ebbe53bedfac
snezhana_seliuk <nana-1980@mail.ru>
2026-05-24 01:50:02 +0300

    add changes to backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic.base import RedirectView
-
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+
 from accounts.views import home_page
 
 urlpatterns = [
