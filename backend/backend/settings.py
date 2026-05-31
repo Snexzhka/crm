d221d463c8590ffc2136cee953e7c4bb3ef8823a
snezhana_seliuk <nana-1980@mail.ru>
2026-06-01 00:45:53 +0300

    add changes to deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,8 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = "django-insecure-2u26=^*-nzm$iui=7@ry0#(wwr64e4%@w0%$f+nbhj^ih*yxni"
 SECRET_KEY = os.getenv('SECRET_KEY')
+
 DEBUG = os.getenv('DEBUG', 'False') == True
+
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
